869655fa9085ced6cbeeec0becd81eeafa3b27ba
Sebastien BOUABE <bouabebastien@gmail.com>
2022-08-04 20:38:38 +0100

    1er commit

--- elements.js
+++ elements.js
@@ -1,69 +1,71 @@
-var ElementNavBar = `<div class="nav-container glass3">
-    <nav class="navbar d-flex align-items-center">
-        <div class="nav-mobile "><a id="nav-toggle" href="#!"><span></span></a></div>
-        <div class="brand">
-            <a href="./index.html"><img src="img/20220728_083028.png" alt="" width="140px"></a>
-        </div>
-        <ul class="nav-list">
-            <li>
-                <a href="index.html">Accueil</a>
-            </li>
-
-            <li>
-                <a href="#!">Département</a>
-                <ul class="nav-dropdown">
-                    <li>
-                        <a href="#!">Numérique</a>
-                        <ul class="nav-dropdown-list">
-                            <li>
-                                <a class="formation-item" href="Ingenieur_logiciel.html">Ingénieur Logiciel</a>
-                            </li>
-                            <li>
-                                <a class="formation-item" href="Ingenieur_Cloud.html">Ingénieur
-                                    Cloud Google</a>
-                            </li>
-                            <li>
-                                <a class="formation-item" href="Ingenieur_FullStack.html">Développeur Full Stack</a>
-                            </li>
-                            <li>
-                                <a class="formation-item" href="Meta.html">Metaverse</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="E-commerce.html">E-commerce</a>
-                    </li>
-
-
-
-
-                    <li>
-                        <a href="Transport.html">Transport</a>
-                    </li>
-                    <li>
-                        <a href="Agritech.html">Agritech</a>
-                    </li>
-                    <li>
-                        <a href="Service_Client.html">Service client</a>
-                    </li>
-                    <li>
-                        <a href="Management_projet.html">Management des projets</a>
-                    </li>
-                    <li>
-                        <a href="Industrie_Culturelle_Creative.html">Industrie culturelle et
-                            créative</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="A_propos.html">A propos</a>
-            </li>
-            <li id="contact">
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-    </nav>
-</div>`;
+var ElementNavBar = `<nav class="navbar glass3">
+<div>
+    <a href="./index.html"><img src="img/20220728_083028.png" alt="" width="140px"></a>
+</div>
+<div>
+<div class="mobile-nav">
+<button class="mobile-btn" onclick="toggleMenu(1)">Menu</button>
+</div>
+<ul class="nav" id="main-nav">
+<li class="close-item">
+<button class="mobile-btn" onclick="toggleMenu(0)">Close &times;</button>
+</li>
+<li class="nav-item">
+<a href="index.html" class="nav-link">Accueil</a>
+</li>
+<li class="nav-item">
+<a href="#!" class="nav-link">
+  Départements <i class="fa fa-caret-down"></i>
+</a>
+<ul class="dp-menu level-1">
+  <li class="dp-item">
+    <a href="#!" class="dp-link">
+      Numérique <i class="fa fa-caret-right"></i>
+    </a>
+    <ul class="dp-menu level-2">
+      <li class="dp-item">
+        <a href="Ingenieur_logiciel.html" class="dp-link">Ingénieur Logiciel</a>
+      </li>
+      <li class="dp-item">
+        <a href="Ingenieur_Cloud.html" class="dp-link">Ingénieur Google Cloud</a>
+      </li>
+      <li class="dp-item">
+        <a href="Ingenieur_FullStack.html" class="dp-link">Développeur Full Stack</a>
+      </li>
+      <li class="dp-item">
+        <a href="Meta.html" class="dp-link">Métaverse</a>
+      </li>
+    </ul>
+  </li>
+  <li class="dp-item">
+    <a href="E-commerce.html" class="dp-link">E-commerce</a>
+  </li>
+  <li class="dp-item">
+    <a href="Transport.html" class="dp-link">Transport</a>
+  </li>
+  <li class="dp-item">
+    <a href="Agritech.html" class="dp-link">Agritech</a>
+  </li>
+  <li class="dp-item">
+    <a href="Service_Client.html" class="dp-link">Service Client</a>
+  </li>
+  <li class="dp-item">
+    <a href="Industrie_Culturelle_Creative.html" class="dp-link">Industrie Culturelle et Créative</a>
+  </li>
+  <li class="dp-item">
+    <a href="Management_projet.html" class="dp-link">Management de projet</a>
+  </li>
+</ul>
+</li>
+<li class="nav-item">
+<a href="A_propos.html" class="nav-link">A propos</a>
+</li>
+<li class="nav-item" id="contact">
+<a href="#!" class="nav-link">Contact</a>
+</li>
+</ul>
+</div>
+</nav>`;
 
 var ElementFooter = `<footer>
     <div class="container pt-5 pb-4">
@@ -92,7 +94,7 @@ var ElementFooter = `<footer>
                 <p><a href="Transport.html">Transport</a></p>
                 <p><a href="Agritech.html">Agritech</a></p>
                 <p><a href="Service_Client.html">Service client</a></p>
-                <p><a href="Management_projet.html">Management des projets</a></p>
+                <p><a href="Management_projet.html">Management de projet</a></p>
                 <p><a href="Industrie_Culturelle_Creative.html">Industrie culturelle créative</a></p>
             </div>
             <div class="col-lg-3">
@@ -155,13 +157,13 @@ var ElementPart = `
     <div class="owl-carousel owl-theme container">
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="img/Ogooue-Labs-1-1024x421.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="img/20220728_012439.jpg" alt="" class="img-fluid">
             </div>
         </div>
@@ -170,68 +172,68 @@ var ElementPart = `
 
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="img/Qwasar-Logo-07.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="img/kisspng-google-logo-googleplex-google-search-5b2cd7fd2d3570.5263877415296655331852-removebg-preview.png"
                     alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/1.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/AIC.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/AUTO.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/Cercle.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/DAL.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/ENoLL.jpg" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/EPIC.jpg" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/Net.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="card glass item" data-aos="flip-left" data-aos-easing="ease-out-cubic">
-            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem">
+            <div class="card-img" style="background-color: white;border-radius:15px; padding:1rem;">
                 <img src="./img/logo/NVIDIA.png" alt="" class="img-fluid">
             </div>
         </div>
